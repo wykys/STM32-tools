972d9056f3c9ab6aee415623dba3aaea7ba2b358
wykys <vvykys@gmail.com>
2018-02-24 00:26:11 +0100

    added C++ support

--- modify_makefile.py
+++ modify_makefile.py
@@ -13,12 +13,51 @@ prog:
 \t{} -c port=SWD reset=HWrst -w build/$(TARGET).bin 0x08000000 -v -rst
 """
 
+CMD_OBJECTS_APPEND_CPP = \
+"""
+OBJECTS += $(addprefix $(BUILD_DIR)/,$(notdir $(CPP_SOURCES:.cpp=.o)))
+vpath %.cpp $(sort $(dir $(CPP_SOURCES)))
+"""
+
+CMD_BUILD_CPP = \
+"""
+$(BUILD_DIR)/%.o: %.cpp Makefile | $(BUILD_DIR)
+\t$(CC) -c $(CFLAGS) -Wa,-a,-ad,-alms=$(BUILD_DIR)/$(notdir $(<:.cpp=.lst)) $< -o $@
+"""
+
+CFLAGS = (
+    '$(MCU)',
+    '-std=c++11',
+    '-Wno-write-strings',
+    '-specs=nano.specs',
+    '-specs=nosys.specs',
+    '$(C_DEFS)',
+    '$(C_INCLUDES)',
+    '$(OPT)',
+    '-Wall',
+    '-fdata-sections',
+    '-ffunction-sections',
+)
+
+LDFLAGS = (
+    '$(MCU)',
+    '-Wl,--no-wchar-size-warning',
+    '-specs=nosys.specs',
+    '-specs=nano.specs',
+    '-T$(LDSCRIPT)',
+    '$(LIBDIR)',
+    '$(LIBS)',
+)
+
 TAG_GENERIC = '# Generic Makefile (based on gcc)'
 TAG_MODIFY_CPP = '# Modified for C++'
 TAG_SOURCES_PATH = '# source path'
 TAG_SOURCES_C = '# C sources'
 TAG_SOURCES_ASM = '# ASM sources'
 TAG_SOURCES_C_INC = '# C includes'
+TAG_LIST_OF_OBJECTS = '# list of objects'
+TAG_LIST_OF_CPP_OBJECTS = '# list of C++ objects'
+TAG_LIFT_OF_ASM_OBJECTS = '# list of ASM program objects'
 TAG_EOF = '# *** EOF ***'
 
 class Makefile(object):
@@ -40,6 +79,9 @@ class Makefile(object):
 
     def get_position_behind(self, expression: str) -> int:
         return self.makefile.find(expression) + len(expression) + 1
+
+    def flags(self, list_of_flags: tuple) -> str:
+        return ''.join(map(lambda x: x + ' ', list_of_flags))[:-1]
 
     def set_variable(self, name: str, value: str):
         position_start = self.get_position_behind(name + ' ')
@@ -78,6 +120,15 @@ class Makefile(object):
         self.replace('$(BINPATH)/', '$(BINPATH)')
         self.set_variable('BINPATH', '/opt/gcc-arm-none-eabi/bin/')
 
+    def support_cpp(self):
+        self.set_variable('CC', '$(BINPATH)$(PREFIX)g++')
+        self.set_variable('CFLAGS', self.flags(CFLAGS))
+        self.set_variable('LDFLAGS', self.flags(LDFLAGS))
+        position = self.makefile.find(TAG_LIFT_OF_ASM_OBJECTS)
+        self.makefile = self.makefile[:position] + TAG_LIST_OF_CPP_OBJECTS + CMD_OBJECTS_APPEND_CPP + self.makefile[position:]
+        position = self.makefile.find('$(BUILD_DIR)/%.o: %.s Makefile | $(BUILD_DIR)') - 1
+        self.makefile = self.makefile[:position] + CMD_BUILD_CPP + self.makefile[position:]
+
     def hide_command(self, cmd: str):
         self.replace('\t' + cmd, '\t@' + cmd)
 
@@ -105,6 +156,7 @@ class Makefile(object):
         self.repair_multiple_definition(TAG_SOURCES_C)
         self.repair_multiple_definition(TAG_SOURCES_C_INC)
         self.update_toolchain()
+        self.support_cpp()
         self.set_variable('OPT', '-Os')
         self.hide_command('$(CC)')
         self.hide_command('$(AS)')
